dde9550e4836f5ead4556973798d89c53ca2bdd4
Zach Tgood <74748020+tg603@users.noreply.github.com>
2022-08-25 10:43:16 -0400

    Add files via upload

---
+++ Main_Workbench/Initial_Testing/move.bat
@@ -0,0 +1,8 @@
+@echo off
+set thing=%date:~4,2%%date:~7,2%%date:~10,4%
+
+cd C:\Users\zachary.t.thoroughgo\DES\Testing
+
+move DESPurple%thing%.csv S:\ARD-Atmospheric\User\WorkingFiles\Thoroughgood
+
+exit
---
+++ Main_Workbench/Initial_Testing/starting.bat
@@ -0,0 +1,6 @@
+@echo off
+cd "C:\Users\zachary.t.thoroughgo\DES\Testing>"
+
+start python Demo.py
+
+exit
